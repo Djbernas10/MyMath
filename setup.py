d43f0905d981c98d50930d7a158d12d10914e2b3
Djbernas10 <bernardo.gomes2011@outlook.com>
2021-06-09 14:50:27 +0100

    Added some simple math functions

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name="MyMath",
+    version="0.1",
+    description="MyMath Package - Comes with 4 simple math function",
+    packages=find_packages(),
+    include_package_data=True
+)
---
+++ teste.py
@@ -0,0 +1,11 @@
+from MyMath import add
+from MyMath import fibonacci,squareroot
+
+num = fibonacci(3)
+print(num)
+
+num2 = squareroot(4)
+print(num2)
+
+soma = add(5,5)
+print(f"a soma é {soma} :) ")
